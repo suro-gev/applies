da4b0f0d324e12f1069414302a229e68b357b0fd
Suro <suro.gevorkyan@bk.ru>
2018-06-12 18:54:04 +0400

    added User class added UserManager class

---
+++ Chat App/Managers/UserManager.swift
@@ -0,0 +1,14 @@
+//
+//  UserManager.swift
+//  Chat App
+//
+//  Created by Suren Gevorkyan on 6/12/18.
+//  Copyright © 2018 Suren Gevorkyan. All rights reserved.
+//
+
+import Foundation
+
+
+class UserManager {
+    var currentUser: User?
+}
